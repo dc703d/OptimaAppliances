aa825d5385577a80b1018c76dedf46b3b1b5e1a3
Daim Choudhary <dc@Daims-MBP.broadband>
2024-12-16 23:31:03 +0000

    trying to fix header background not loading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: "url('/generated_background2.jpg')"}} id='Header'>
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: `url('/generated_background.jpg')`}} id='Header'>
       <Navbar/>
       <motion.div 
       initial={{opacity: 0, y: 100}}
