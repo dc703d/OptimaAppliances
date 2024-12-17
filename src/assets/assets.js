6c77b27cc633706c95e08ed932df0d2b6b744c71
Daim Choudhary <dc@Daims-MBP.broadband>
2024-12-17 00:17:11 +0000

    Add GitHub Actions workflow for GitHub Pages deployment

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -20,16 +20,9 @@ import dishwasher from './Dishwasher.jpg'
 
 export const assets = {
     logo,
-    logo_dark,
     cross_icon,
     menu_icon,
     star_icon,
-    header_img,
-    brand_img,
-    project_img_1,
-    project_img_2,
-    project_img_3,
-    project_img_4,
     left_arrow,
     right_arrow,
     logo_png,
